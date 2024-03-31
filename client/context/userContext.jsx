c15f3092f62324e829aaa309c921d1bb87eb7ea4
Abdul Moeez Asad <113182819+AMoeez1910@users.noreply.github.com>
2024-03-31 05:50:17 +0500

    Update userContext.jsx

--- client/context/userContext.jsx
+++ client/context/userContext.jsx
@@ -4,15 +4,15 @@ export const UserContext = createContext({})
 export function UserContextProvider({children}){
     const [user,setUser] = useState(null);
     // if user or not
-//     useEffect(()=>{
-//     // if(!user){
-//     //     axios.get('https://fund-raiser-production.up.railway.app/profile')
-//     //     .then(res => {
-//     //       if(res.data)
-//     //         setUser(res.data)
-//     //       })
-// }
-//     },[user])
+    useEffect(()=>{
+    if(!user){
+        axios.get('https://fund-raiser-production.up.railway.app/profile')
+        .then(res => {
+          if(res.data)
+            setUser(res.data)
+          })
+}
+    },[user])
     return (
         <UserContext.Provider value={{user,setUser}}>
             {children}
