032b9c729e6a1184213f6888aa3eff411fe6e6f8
Abdul Moeez Asad <113182819+AMoeez1910@users.noreply.github.com>
2024-03-31 15:32:08 +0500

    Update FundRaiser.jsx

--- client/src/pages/FundRaiser.jsx
+++ client/src/pages/FundRaiser.jsx
@@ -13,21 +13,21 @@ const FundRaiser = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
   axios.defaults.withCredentials=true
-  useEffect(() => {
-    axios.get(`https://fund-raiser-production.up.railway.app/profile`)
-    .then(res => {
+  // useEffect(() => {
+  //   axios.get(`https://fund-raiser-production.up.railway.app/profile`)
+  //   .then(res => {
       
-      if(res.data)
-    {if(res.data.Status === "Success"){
-        setIsLoggedIn(true)
-        setID(res.data.id)
-        getLoggedIn(true)
-      }
-      else{
-        setIsLoggedIn(false)
-      }}
-    })
-  }, [isLoggedIn]);
+  //     if(res.data)
+  //   {if(res.data.Status === "Success"){
+  //       setIsLoggedIn(true)
+  //       setID(res.data.id)
+  //       getLoggedIn(true)
+  //     }
+  //     else{
+  //       setIsLoggedIn(false)
+  //     }}
+  //   })
+  // }, [isLoggedIn]);
   const handleNext = (...keyValuePairs) => {
     setData((prevData) => ({
       ...prevData,
